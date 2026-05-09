85be47ce50c262839800a2d00890d730f20845eb
Mahae Koh <flomatiks@gmail.com>
2026-05-09 05:57:53 -0700

    Fix linux builds

--- crates/cargo-truce/src/commands/package/mod.rs
+++ crates/cargo-truce/src/commands/package/mod.rs
@@ -126,6 +126,7 @@ impl PkgFormat {
         s.split(',').map(|t| t.trim().parse()).collect()
     }
 
+    #[cfg_attr(not(any(target_os = "macos", target_os = "windows")), allow(dead_code))]
     pub(crate) fn label(&self) -> &'static str {
         match self {
             PkgFormat::Clap => "CLAP",
